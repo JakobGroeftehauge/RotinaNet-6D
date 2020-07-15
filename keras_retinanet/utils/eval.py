bf7a4b2e0708e0c0be3710978f3f820dc2250305
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2020-07-15 11:38:51 +0200

    Fixed problem with not printing predictions on the evaluation images

--- keras_retinanet/utils/eval.py
+++ keras_retinanet/utils/eval.py
@@ -106,10 +106,11 @@ def _get_detections(generator, model, score_threshold=0.05, max_detections=100, 
         image_detections = np.concatenate([image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_labels, axis=1)], axis=1)
 
         if save_path is not None:
-            draw_annotations(raw_image, generator.load_annotations(i), label_to_name=generator.label_to_name)
-            draw_detections(raw_image, image_boxes, image_scores, image_labels, label_to_name=generator.label_to_name, score_threshold=score_threshold)
-            #print(image_boxes)
-            cv2.imwrite(os.path.join(save_path, '{}.png'.format(i)), raw_image)
+            # Copy() is necessary, else the boxes will not be printed on the saved images. 
+            draw_image = raw_image.copy()
+            draw_annotations(draw_image, generator.load_annotations(i), label_to_name=generator.label_to_name)
+            draw_detections(draw_image, image_boxes, image_scores, image_labels, label_to_name=generator.label_to_name, score_threshold=score_threshold)
+            cv2.imwrite(os.path.join(save_path, '{}.png'.format(i)), draw_image)
 
         # copy detections to all_detections
         for label in range(generator.num_classes()):
