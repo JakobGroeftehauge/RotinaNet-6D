5bd959547455dbfb9d56da50a7b470231bf59683
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-07-16 10:18:14 +0200

    added tensorboard to notebook

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -162,13 +162,7 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
         )
 
     if args.evaluation and validation_generator:
-        if args.dataset_type == 'coco':
-            from ..callbacks.coco import CocoEval
-
-            # use prediction model for evaluation
-            evaluation = CocoEval(validation_generator, tensorboard=tensorboard_callback)
-        else:
-            evaluation = Evaluate(validation_generator, tensorboard=tensorboard_callback, weighted_average=args.weighted_average)
+        evaluation = Evaluate(validation_generator, tensorboard=tensorboard_callback, weighted_average=args.weighted_average)
         evaluation = RedirectModel(evaluation, prediction_model)
         callbacks.append(evaluation)
 
