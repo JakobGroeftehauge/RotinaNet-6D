2626c4fd1a1b735f4bfd3e82bf21aae2edee64ae
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-07-16 08:44:50 +0200

    Update retinanet.py

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -213,7 +213,7 @@ def pose_submodels(num_classes, num_anchors):
     return [
         ('regression', default_regression_model(4, num_anchors)),
         ('classification', default_classification_model(num_classes, num_anchors)),
-        ('pose', default_regression_model(12,num_anchors))
+        ('pose', default_regression_model(12,num_anchors, name='pose_submodel'))
     ]
 
 
