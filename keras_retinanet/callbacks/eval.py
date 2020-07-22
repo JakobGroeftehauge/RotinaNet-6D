0250e1f88ff1a7097d29d2f93e6d2389c4ae9734
JakobGroeftehauge <JakobGroeftehauge@github.com>
2020-07-22 11:48:06 +0200

    Fixed syntax problems

--- keras_retinanet/callbacks/eval.py
+++ keras_retinanet/callbacks/eval.py
@@ -83,9 +83,9 @@ class Evaluate(keras.callbacks.Callback):
         else:
             self.mean_ap = sum(precisions) / sum(x > 0 for x in total_instances)
         
-        if self.verbose == 1
+        if self.verbose == 1:
             for label, CEP_ratio in CEP_ratios.items():
-                print('Percentage of correctly estimated (ADD) poses of class ', generator.label_to_name(label), ': {:.4f}'.format(CEP_ratio))
+                print('Percentage of correctly estimated (ADD) poses of class ', self.generator.label_to_name(label), ': {:.4f}'.format(CEP_ratio))
 
         if self.tensorboard:
             import tensorflow as tf
