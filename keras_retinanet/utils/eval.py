6858fd5f045feeaddb7b9a506006e88598e7c7b9
JakobGroeftehauge <JakobGroeftehauge@github.com>
2020-08-10 11:52:56 +0200

    Print statement for rotations matrices in ADD eval

--- keras_retinanet/utils/eval.py
+++ keras_retinanet/utils/eval.py
@@ -21,7 +21,7 @@ import keras
 import numpy as np
 import os
 import time
-
+import csv 
 import cv2
 import progressbar
 assert(callable(progressbar.progressbar)), "Using wrong progressbar module, install 'progressbar2' instead."
@@ -60,9 +60,21 @@ def _test_ADD(gt_pose_translation, gt_pose_rotation, detected_pose_translation,
     gt_pose_rotation = gt_pose_rotation.reshape((3,3))
     detected_pose_rotation = detected_pose_rotation.reshape((3,3))
 
+    with open("pre_file.csv") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        det = np.linalg.det(detected_pose_rotation)
+        R1, R2, R3, R4, R5, R6, R7, R8, R9 = detected_pose_rotation.reshape(-1)
+        writer.writerow(det, R1, R2, R3, R4, R5, R6, R7, R8, R9)
+
     U, S, V_t = np.linalg.svd(detected_pose_rotation, full_matrices=True)
     det = np.sign(np.linalg.det(np.matmul(V_t.T,U.T)))
     detected_pose_rotation = np.matmul(np.matmul(V_t.T, np.array([[1,0,0],[0,1,0],[0,0,det]])), U.T)
+
+    with open("post_file.csv") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        det = np.linalg.det(detected_pose_rotation)
+        R1, R2, R3, R4, R5, R6, R7, R8, R9 = detected_pose_rotation.reshape(-1)
+        writer.writerow(det, R1, R2, R3, R4, R5, R6, R7, R8, R9)
 
     newPL_ori = np.transpose( np.matmul(gt_pose_rotation, np.transpose(point_cloud_test)) )
     #newPL_ori = newPL_ori + gt_pose_translation*1000
