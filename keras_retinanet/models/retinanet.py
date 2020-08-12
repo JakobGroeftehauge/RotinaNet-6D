49e6376caf1606fb20abfb02245b99c9d6ce9636
JakobGroeftehauge <JakobGroeftehauge@github.com>
2020-08-12 09:47:07 +0200

    ...

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -169,9 +169,9 @@ def default_pose_model(num_values, num_anchors, pyramid_feature_size=256, regres
     trans = layers.ExtractTranslation()(outputs)
     rot = keras.layers.Reshape((-1, 3,3), name='pyramid_rotation_reshape')(rot)
     #inputs = keras.layers.Reshape((-1), name="reshape")(inputs)
-    inputs_mat = keras.layers.Reshape((-1, 3, 3))(inputs)
+    #inputs_mat = keras.layers.Reshape((-1, 3, 3))(inputs)
     rot = layers.Reorthogonalize()(rot)
-    rot = keras.layers.Reshape(-1, 9)(rot)
+    rot = keras.layers.Reshape((-1, 9))(rot)
     trans = keras.layers.Reshape((-1, 3), name='pyramid_translation_reshape')(trans)
 
     return keras.models.Model(inputs=inputs, outputs=[rot, trans], name=name)
