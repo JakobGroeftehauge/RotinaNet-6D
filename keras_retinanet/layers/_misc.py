30759f63aa4bca16e3c9b8e9ca9a0d18d109c30c
JakobGroeftehauge <JakobGroeftehauge@github.com>
2020-08-12 20:53:36 +0200

    ScaleDeterminant

--- keras_retinanet/layers/_misc.py
+++ keras_retinanet/layers/_misc.py
@@ -234,7 +234,7 @@ class ScaleDeterminant(keras.layers.Layer):
         print("inputs: ", inputs.shape)
         det = tf.linalg.det(inputs)
         #inputs = tf.reshape(inputs, (-1, 9))
-        inputs = tf.math.multiply(inputs, tf.reshape(tf.divide(1.0, tf.transpose(det)), shape=-1))
+        inputs = tf.math.multiply(inputs, tf.reshape(tf.divide(1.0, tf.transpose(det)), shape=(-1, 1)))
         #inputs = tf.reshape(inputs, (-1, 3, 3))
         #print("inputs after: ", inputs.shape)
         return inputs
