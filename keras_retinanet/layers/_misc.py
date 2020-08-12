e7421b348cbd30b73563d82fb16dab31d2bbf95e
JakobGroeftehauge <JakobGroeftehauge@github.com>
2020-08-12 08:19:20 +0200

    Reorthogonalize modifications

--- keras_retinanet/layers/_misc.py
+++ keras_retinanet/layers/_misc.py
@@ -216,8 +216,8 @@ class Reorthogonalize(keras.layers.Layer):
         #x, y, z = np.shape(inputs)
 
         U, S, V_t = tf.linalg.svd(inputs)
-        det = np.sign(np.linalg.det(np.matmul(tf.transpose(V_t), tf.transpose(U))))
-        ort_rotation = np.matmul(np.matmul(tf.transpose(V_t), np.array([[1,0,0],[0,1,0],[0,0,det]])), tf.transpose(U))
+        det = np.sign(tf.linalg.det(tf.matmul(tf.transpose(V_t), tf.transpose(U))))
+        ort_rotation = tf.matmul(tf.matmul(tf.transpose(V_t), np.array([[1,0,0],[0,1,0],[0,0,det]])), tf.transpose(U))
 
         return ort_rotation
 
